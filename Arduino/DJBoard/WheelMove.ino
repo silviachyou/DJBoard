7b9092ee58333e2f4831185d9d7ae1470ef80dc2
yafangshih <avanshih@gmail.com>
2015-06-11 20:46:17 +0800

    comments added

--- Arduino/DJBoard/WheelMove.ino
+++ Arduino/DJBoard/WheelMove.ino
@@ -22,33 +22,34 @@ void checkWheelMove(){
      digitalWrite(led,HIGH);
      
      isBlack = false;
-     dis = dis + wheelLen;
+     /* distance */
+     dis = dis + wheelLen; // += 15cm
      Serial.print("distance: ");
      Serial.println(dis);
      char dString[80]="dis= ";
      char sofd[20];
-     
      dtostrf(dis, 8, 0, sofd);
      strcat(dString, sofd);  
      strcat(dString, eol);
-     Bluetooth.write(dString);
+     Bluetooth.write(dString); // cm
 
-     if(!isMoving){
+     if(!isMoving){ // previous state: stop 
       Serial.println("Board starts moving");
       Bluetooth.write(playWheelMusic);
       isMoving = true;
     }
-    else{
+    else{ // previous state: moving
       Serial.println("Board keeps moving");
       duration = millis()-time;
       Serial.print("duration: ");
       Serial.println(duration);
-      v = (double)wheelLen*10/(double)duration;
+      
+      /* velocity */
+      v = (double)wheelLen*10/(double)duration; // cm/millis -> m/s
       Serial.print("velocity: ");
       Serial.println(v);
       char vString[80]="v= ";
       char sofv[20];
-      
       dtostrf(v, 6, 4, sofv);
       strcat(vString, sofv);  
       strcat(vString, eol);
@@ -56,11 +57,11 @@ void checkWheelMove(){
     }
     time = millis();
   }
-  else if(IRVal==LOW) {  //isBlack
+  else if(IRVal==LOW) {  //isBlack 
     digitalWrite(led,LOW);
     isBlack = true;
   }
-  if(millis()-time>1000 && isMoving){
+  if(millis()-time>1000 && isMoving){ // stop
      // Serial.println("Board Stopped");
       Bluetooth.write(stopWheelMusic);
       isMoving = false;
