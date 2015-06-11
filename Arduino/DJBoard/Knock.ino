94e03aadb95a7eeb2b4cbf3db8a94b78d0680cb8
Yen <yen@flux3dp.com>
2015-06-11 22:59:47 +0800

    change to pressure sensor

--- Arduino/DJBoard/Knock.ino
+++ Arduino/DJBoard/Knock.ino
@@ -6,18 +6,18 @@ bool knockStatus2 = 0;
 bool knockStatus3 = 0;
 
 void checkKnock(){
-   int sensorReading1 = 0; 
+   int sensorReading1 = 0;
    int sensorReading2 = 0;
    int sensorReading3 = 0;
-   sensorReading1 = digitalRead(knockSensor1);
-   sensorReading2 = digitalRead(knockSensor2);
-   sensorReading3 = digitalRead(knockSensor3);
+   sensorReading1 = analogReadRead(knockSensor1);
+   sensorReading2 = analogReadRead(knockSensor2);
+   sensorReading3 = analogReadRead(knockSensor3);
 
    if(millis() - knocktime > 1000) {
      if(knockStatus1 != sensorReading1) {
        knocktime = millis();
        knockStatus1 = sensorReading1;
-       if(knockStatus1 == 0) {
+       if(knockStatus1 >= 80) {
          Serial.println("Knock Front");
          Bluetooth.write(knockFront);
        }
@@ -25,7 +25,7 @@ void checkKnock(){
      if(knockStatus2 != sensorReading2) {
        knocktime = millis();
        knockStatus2 = sensorReading2;
-       if(knockStatus2 == 0) {
+       if(knockStatus2 >= 80) {
          Serial.println("Knock Mid");
          Bluetooth.write(knockMid);
        }
@@ -33,7 +33,7 @@ void checkKnock(){
      if(knockStatus3 != sensorReading3) {
        knocktime = millis();
        knockStatus3 = sensorReading3;
-       if(knockStatus3 == 0) {
+       if(knockStatus3 >= 80) {
          Serial.println("Knock Back");
          Bluetooth.write(knockBack);
        }
